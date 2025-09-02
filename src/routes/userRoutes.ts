6782b071b694a08e7b58187c0b719a607a7a12fc
kleberson154 <kleberson55@hotmail.com>
2025-09-02 14:54:46 -0300

    usando os middlewares nas rotas

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import User, { IUser } from '../models/User'
 import UserController from '../Controller/UserController'
+import { authMiddleware } from '../middlewares/authMiddleware'
 
 const router: Router = Router()
 
@@ -12,15 +13,19 @@ router.get('/login', async (req, res) => {
   await UserController.login(req, res)
 })
 
-router.post('/deposit', async (req, res) => {
+router.get('/home', authMiddleware, async (req, res) => {
+  res.json({ message: 'Bem-vindo à sua conta!' })
+})
+
+router.post('/deposit', authMiddleware, async (req, res) => {
   await UserController.deposit(req, res)
 })
 
-router.post('/withdraw', async (req, res) => {
+router.post('/withdraw', authMiddleware, async (req, res) => {
   await UserController.withdraw(req, res)
 })
 
-router.post('/transfer', async (req, res) => {
+router.post('/transfer', authMiddleware, async (req, res) => {
   await UserController.transfer(req, res)
 })
 
